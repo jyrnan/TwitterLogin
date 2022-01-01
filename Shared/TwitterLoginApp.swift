e1de961a74513dae9c9db17463f5798dfe4ede7f
jyrnan <yoeking@gmail.com>
2022-01-01 09:28:17 +0800

    Initial Commit

---
+++ Shared/TwitterLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterLoginApp.swift
+//  Shared
+//
+//  Created by Yong Jin on 2022/1/1.
+//
+
+import SwiftUI
+
+@main
+struct TwitterLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
